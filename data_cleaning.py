35ba3789170f4f1de7029114c9ad3c72f60185d2
Jens Tinggaard <mail@t1ng.dk>
2024-09-26 13:28:12 +0200

    Specify linter rules and automate VSCode setup

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 # Loading the orignal data into a pandas dataframe
 file_path = "trefor.csv"
-df = pd.read_csv(file_path)
+consumption = pd.read_csv(file_path)
 
 # Dropping date and time, as this is not needed when cleaning
-df_original = df.drop(columns=["Dato", "Time"])
+df_original = consumption.drop(columns=["Dato", "Time"])
 datapoint_count = df_original.count().sum()
 
 # Copy is disconnected from the orignal df
@@ -21,7 +21,7 @@ print(f"Original datapoints: {datapoint_count}")
 print(f"Indicies removed: {datapoint_count - cleaned.count().sum()}")
 
 # Reads the date and time before transfering to csv file
-cleaned = pd.concat([df[["Dato", "Time"]], cleaned], axis=1)
+cleaned = pd.concat([consumption[["Dato", "Time"]], cleaned], axis=1)
 
 # Return the cleaned data into another csv
 cleaned.to_csv("trefor_cleaned.csv", sep=";", decimal=",", index=False)
