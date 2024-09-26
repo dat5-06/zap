e9d2064d97e3b8d049d01a8ab02fa178f8abc5ff
Jens Tinggaard <mail@t1ng.dk>
2024-09-26 09:08:40 +0200

    Use lambda functions to omit zeros and large numbers

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,38 +1,27 @@
 import pandas as pd
-import numpy as np
-from data_cleaning_functions import find_first_non_zero, find_last_non_zero
 
 # Loading the orignal data into a pandas dataframe
-file_path = 'trefor.csv'
+file_path = "trefor.csv"
 df = pd.read_csv(file_path)
 
 # Dropping date and time, as this is not needed when cleaning
-df_original = df.drop(columns=['Dato', 'Time'])
+df_original = df.drop(columns=["Dato", "Time"])
+datapoint_count = df_original.count().sum()
 
 # Copy is disconnected from the orignal df
 df_cleaning = df_original.copy(deep=True)
 
-# For each column we find the first non zero and the last non zero and stores all indicies in a df
-first_non_zero_idx = df_cleaning.apply(find_first_non_zero)
-last_non_zero_idx = df_cleaning.apply(find_last_non_zero)
-
-for column in df_cleaning.columns:
-    # We find the indicies for this specific column
-    first_non_zero = first_non_zero_idx[column]
-    last_non_zero = last_non_zero_idx[column]
-    
-    # Replaces beginning zeros and ending zeros with nan if any
-    if first_non_zero is not None and last_non_zero is not None:
-        df_cleaning.loc[:first_non_zero-1, column] = np.nan
-        df_cleaning.loc[last_non_zero+1:, column] = np.nan
+# Only take valuse that are greater than 0
+non_zeroes = df_cleaning.apply(lambda series: series[series > 0])
+# And only take values that are less than 100
+cleaned = non_zeroes.apply(lambda series: series[series < 100])
 
 # Print the removed datapoints
-print(f'Original datapoints: {df_original.count().sum()}')
-print(f'Cleaned datapoints: {df_cleaning.count().sum()}')
-print(f'Datapoints removed: {df_original.count().sum() - df_cleaning.count().sum()}')
+print(f"Original datapoints: {datapoint_count}")
+print(f"Indicies removed: {datapoint_count - cleaned.count().sum()}")
 
-# Readds the date and time before transfering to csv file
-df_cleaning = pd.concat([df[['Dato', 'Time']], df_cleaning], axis=1)
+# Reads the date and time before transfering to csv file
+cleaned = pd.concat([df[["Dato", "Time"]], cleaned], axis=1)
 
 # Return the cleaned data into another csv
-df_cleaning.to_csv('trefor_cleaned.csv', index=False)
+cleaned.to_csv("trefor_cleaned.csv", sep=";", decimal=",", index=False)
--- data_cleaning_functions.py
+++ data_cleaning_functions.py
@@ -2,14 +2,15 @@
 def find_first_non_zero(series):
     non_zero_indices = series[series > 0].index
     if len(non_zero_indices) > 0:
-        return non_zero_indices[0] 
+        return non_zero_indices[0]
     else:
-        return None  
+        return None
+
 
 # Finds all data greater than 0 and returns the last index of where a datapoint that isn't zero occurs
 def find_last_non_zero(series):
     non_zero_indices = series[series > 0].index
     if len(non_zero_indices) > 0:
-        return non_zero_indices[-1]  
+        return non_zero_indices[-1]
     else:
         return None
