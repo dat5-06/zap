5587c661e019492b08d5a6a6e2e4e14775d77b71
askeko <askeklitgaard@gmail.com>
2024-10-17 13:33:15 +0200

    ML-144 first experiment, gru
    co-authored-by: Moartin <enevoldsen2oo@gmail.com>
    co-authored-by: Jons <mail@t1ng.dk>

--- core/util/loss_functions.py
+++ core/util/loss_functions.py
@@ -1,19 +1,14 @@
 import torch
 
 
-def zap_loss(y_pred: torch.tensor, y_true: torch.tensor) -> torch.tensor:
+def zap_loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
     """Loss function that considers spikes Mean(e^(5*y_true)*(y_pred-y_true))."""
-    base_loss = torch.mean(
-        torch.multiply(
-            torch.exp(torch.multiply(5, y_true)),
-            torch.square(torch.subtract(y_pred, y_true)),
-        )
-    )
+    base_loss = torch.mean(torch.exp(5 * y_true) * torch.square(y_pred - y_true))
 
     # Penalty for negative predictions
-    negative_penalty = torch.mean(torch.relu(-y_pred))  # Penalizes when y_pred < 0
+    negative_penalty = torch.mean(torch.relu(-y_pred))
 
-    # Adjust the weight of the penalty as needed (e.g., 10.0 can be modified)
-    total_loss = base_loss + 10 * negative_penalty
+    # Adjust the weight of the penalty as needed (e.g., 5 can be modified)
+    total_loss = base_loss + 5 * negative_penalty
 
     return total_loss
