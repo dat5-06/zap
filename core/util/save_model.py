35e71e6bf50836039739d63a2da775bd28b2dfab
Markus Vang Kleist <mkleis22@student.aau.dk>
2024-10-30 10:55:51 +0100

    Updated save models so constructor input no longer is required

--- core/util/save_model.py
+++ core/util/save_model.py
@@ -4,9 +4,7 @@ import pandas as pd
 from core.util.io import get_project_root
 
 
-def save_model(
-    model: nn.Module, model_name: str, overwrite: bool = False, *args: int
-) -> None:
+def save_model(model: nn.Module, model_name: str, overwrite: bool = False) -> None:
     """Save a model's parameters for later use.
 
     Arguments:
@@ -14,33 +12,29 @@ def save_model(
         model (nn.Module): model that should be saved
         model_name (str): name of the file without filetype
         overwrite (bool): if the entry should be overwritten if it already exists
-        *args (int): constructor arguments used to initialise the model
 
     """
     model_path = get_project_root() / "core/models/saved_models"
 
-    file = pd.read_csv(model_path / "constructor_args.csv", sep=";")
+    arg_file = pd.read_csv(model_path / "constructor_args.csv", sep=";")
     index = -1
 
     # Check if the model already exists and get its index if it does
-    if model_name in file.get("Model name").to_list():
+    if model_name in arg_file.get("Model name").to_list():
         if overwrite:
-            index = file.index[file["Model name"] == model_name][0]
+            index = arg_file.index[arg_file["Model name"] == model_name][0]
         else:
             raise Exception("Model name already exists")
 
-    # Make constructor arguments to a list
-    constructor_arguments = []
-    for arg in args:
-        constructor_arguments.append(str(arg))
-
-    # Convert the list to a single string for storage
-    constructor_arguments = " ".join(constructor_arguments)
+    # Convert member values to strings for storage
+    constructor_arguments = model.get_members()
+    for i in range(len(constructor_arguments)):
+        constructor_arguments[i] = str(constructor_arguments[i])
 
     # Add or change row of the model
-    file.loc[index, "Model name"] = model_name
-    file.loc[index, "Parameters"] = constructor_arguments
-    file.to_csv(model_path / "constructor_args.csv", index=False, sep=";")
+    arg_file.loc[index, "Model name"] = model_name
+    arg_file.loc[index, "Parameters"] = " ".join(constructor_arguments)
+    arg_file.to_csv(model_path / "constructor_args.csv", index=False, sep=";")
 
     # Save the model as a file
     torch.save(model.state_dict(), model_path / f"{model_name}.pt")
@@ -53,20 +47,21 @@ def load_model(model_class: nn.Module, model_name: str, device: str) -> nn.Modul
     ---------
         model_class (nn.Module): class of the model
         model_name (str): name of model in saved_models folder without filetype
-        device (str): device being used (e.g. cuda:0)
+        device (str): device being used (e.g. "cuda:0")
 
     """
     model_path = get_project_root() / "core/models/saved_models"
 
     # Check that the model exists
-    file = pd.read_csv(model_path / "constructor_args.csv", sep=";")
-    if model_name not in file.get("Model name").to_list():
+    arg_file = pd.read_csv(model_path / "constructor_args.csv", sep=";")
+    if model_name not in arg_file.get("Model name").to_list():
         raise Exception("Model parameters doesn't exist")
 
     # Get parameter values of the model
-    parameters = file.loc[file["Model name"] == model_name].to_numpy()[0].tolist()
+    parameters = (
+        arg_file.loc[arg_file["Model name"] == model_name].to_numpy()[0].tolist()
+    )
     parameters = parameters[1]
-
     parameters = parameters.split()
 
     # Convert elements to integers
