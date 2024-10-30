7ef00f232e1cfc7e7b3cba2c60fded8ab365a213
Markus Vang Kleist <mkleis22@student.aau.dk>
2024-10-30 13:53:05 +0100

    ML-163 Early stop class

---
+++ core/util/early_stop.py
@@ -0,0 +1,30 @@
+class EarlyStop:
+    """Class for implementing early stop."""
+
+    def __init__(self, patience: int, min_delta: float = 0) -> None:
+        """Initialise early stop object.
+
+        Arguments:
+        ---------
+            patience (int): how many epochs without improvement before it early stops
+            min_delta (float): slack for how much worse the new loss can be
+
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float("inf")
+
+    def early_stop(self, validation_loss: float) -> bool:
+        """Check if the model should early stop."""
+        # Check if it has gotten a better validation loss
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        # Check if the validation loss is greater than the allowed slack (min_val+delta)
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            # Stops if patience runs out
+            if self.counter >= self.patience:
+                return True
+        return False
