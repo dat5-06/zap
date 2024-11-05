ab473da297ceb11da1c2f4ddf9a8f7efbe63d534
maxnm47 <maxnm47@gmail.com>
2024-11-05 14:01:39 +0100

    added 24 hour lag, this took way too long

---
+++ core/util/lag24_baseline.py
@@ -0,0 +1,32 @@
+import torch
+
+
+def avg_loss(
+    y: torch.Tensor, naive_predictor: callable, loss_function: callable
+) -> float:
+    """Calculate average loss for regression function."""
+    total_loss = 0.0
+    n_samples = len(y)
+    for i in range(n_samples):
+        y_pred = naive_predictor(y, i)
+        total_loss += loss_function(y_pred, y[i]).item()
+    return total_loss / n_samples
+
+
+def lag24_baseline(
+    loss_func: callable,
+    y_test: torch.Tensor,
+    model_loss: float,
+    n_lag: int = 24,
+) -> float:
+    """Calculate the percentage loss of the lag24 baseline."""
+
+    def naive_predictor(y: torch.Tensor, idx: int) -> torch.Tensor:
+        return y[idx - n_lag]
+
+    return avg_loss(y_test, naive_predictor, loss_func) / model_loss
+
+
+def naive_predictor(y: torch.Tensor, idx: int) -> torch.Tensor:
+    """Predict the value of y at index idx using the lag24 baseline."""
+    return y[idx - 24]
