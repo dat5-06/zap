1b55c86feaa6e01a713bf268ac87dee638545377
maxnm47 <maxnm47@gmail.com>
2024-11-06 11:27:40 +0100

    added tensor support for naive linear baseline

--- core/util/naive_loss.py
+++ core/util/naive_loss.py
@@ -1,9 +1,10 @@
-import math
-from core.util.io import read_csv
+import torch
 from scipy import stats
 
 
-def avg_loss(x: list, y: list, regression: callable, loss_function: callable) -> float:
+def avg_loss_linear(
+    x: torch.Tensor, y: torch.Tensor, regression: callable, loss_function: callable
+) -> float:
     """Calculate average loss for regression function."""
     loss = 0
     for i in range(len(x)):
@@ -11,26 +12,17 @@ def avg_loss(x: list, y: list, regression: callable, loss_function: callable) ->
     return loss / len(x)
 
 
-def get_percentage_loss(loss_func: callable, model_loss: float) -> float:
+def naive_linear_baseline(
+    loss_func: callable,
+    y_test: torch.Tensor,
+    x_test: torch.Tensor,
+    model_loss: float,
+) -> float:
     """Find percentage difference between naive model loss and the inputted loss."""
-    normalised_data = read_csv("processed/park_timeseries_lin.csv")
-
-    # Names of all park titles
-    columns = normalised_data.columns.to_list()
-
-    # Adds all values to coordinate lists x and y
-    x, y = [], []
-    for index, row in normalised_data.iterrows():
-        for title in columns:
-            if not math.isnan(row[title]):
-                x.append(index)
-                y.append(row[title])
-
-    # Makes linear regression
-    slope, intercept, _, _, _ = stats.linregress(x, y)
+    slope, intercept, _, _, _ = stats.linregress(x_test.flatten(), x_test.flatten())
 
     def regression(x: float) -> float:
         """Make a function for the regression."""
         return slope * x + intercept
 
-    return avg_loss(x, y, regression, loss_func) / model_loss
+    return avg_loss_linear(x_test, y_test, regression, loss_func) / model_loss
