b013942b064538c52aea7a0709686a770f9e8f5c
tdawg <thomast2001@gmail.com>
2024-11-19 15:25:30 +0100

    added relu before output on lstm and gru to prevent negative values

--- core/util/loss_functions.py
+++ core/util/loss_functions.py
@@ -3,12 +3,4 @@ import torch
 
 def zap_loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
     """Loss function that considers spikes Mean(e^(5*y_true)*(y_pred-y_true))."""
-    base_loss = torch.mean(torch.exp(5 * y_true) * torch.square(y_pred - y_true))
-
-    # Penalty for negative predictions
-    negative_penalty = torch.mean(torch.relu(-y_pred))
-
-    # Adjust the weight of the penalty as needed (e.g., 5 can be modified)
-    total_loss = base_loss + 5 * negative_penalty
-
-    return total_loss
+    return torch.mean(torch.exp(5 * y_true) * torch.square(y_pred - y_true))
