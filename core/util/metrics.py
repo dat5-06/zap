94116a7805c480e661058af15d1c3b4a3bfe54ad
MarkusVK <113092085+MarkusVK@users.noreply.github.com>
2024-11-27 15:12:07 +0100

    Notebook for baseline (#69)

--- core/util/metrics.py
+++ core/util/metrics.py
@@ -5,27 +5,22 @@ def mae(y: list, y_hat: list) -> float:
     """Calculate MAE."""
     sum_tracker = 0
     for i in range(len(y)):
-        for j in range(len(y[i])):
-            sum_tracker += abs(y[i][j] - y_hat[i][j])
-    return float(sum_tracker / (len(y) * 24))
+        sum_tracker += abs(y[i] - y_hat[i])
+    return sum_tracker / len(y)
 
 
 def rmse(y: list, y_hat: list) -> float:
     """Calculate RMSE."""
     sum_tracker = 0
     for i in range(len(y)):
-        for j in range(len(y[i])):
-            sum_tracker += math.pow(y[i][j] - y_hat[i][j], 2)
-    return math.sqrt(sum_tracker / (len(y) * 24))
+        sum_tracker += math.pow(y[i] - y_hat[i], 2)
+    return math.sqrt(sum_tracker / len(y))
 
 
 def smape(y: list, y_hat: list) -> float:
     """Calculate sMAPE."""
     sum_tracker = 0
     for i in range(len(y)):
-        for j in range(len(y[i])):
-            if abs(y[i][j]) + abs(y_hat[i][j]) != 0:
-                sum_tracker += abs(y[i][j] - y_hat[i][j]) / (
-                    (abs(y[i][j]) + abs(y_hat[i][j])) / 2
-                )
-    return float((100 / (len(y) * 24)) * sum_tracker)
+        if abs(y[i]) + abs(y_hat[i]) != 0:
+            sum_tracker += abs(y[i] - y_hat[i]) / ((abs(y[i]) + abs(y_hat[i])) / 2)
+    return (100 / len(y)) * sum_tracker
