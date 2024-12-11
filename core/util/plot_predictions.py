36b97883bc1ad7e9b69eb30a526d91ca8b8db77d
Jens Tinggaard <mail@t1ng.dk>
2024-12-11 15:56:51 +0100

    Include 24h lag in plots :shipit: (#91)

--- core/util/plot_predictions.py
+++ core/util/plot_predictions.py
@@ -29,6 +29,7 @@ def plot_every_model(
     end_day: int,
     y_test: torch.Tensor,
     predicted_naive: np.ndarray,
+    predicted_lagged: np.ndarray,
     predicted_lstm: torch.Tensor,
     predicted_gru: torch.Tensor,
     predicted_cnn_lstm: torch.Tensor,
@@ -42,6 +43,7 @@ def plot_every_model(
 
     ground_truth = y_test[index(start_day)].flatten().to("cpu")
     naive = predicted_naive[index(start_day)].flatten()
+    lagged = predicted_lagged[index(start_day)].flatten()
     lstm = predicted_lstm[index(start_day)].flatten().to("cpu")
     gru = predicted_gru[index(start_day)].flatten().to("cpu")
     cnn_lstm = predicted_cnn_lstm[index(start_day)].flatten().to("cpu")
@@ -49,6 +51,7 @@ def plot_every_model(
     for i in range(start_day + 1, end_day):
         ground_truth = torch.cat((ground_truth, y_test[index(i)].flatten().to("cpu")))
         naive = np.concatenate((naive, predicted_naive[index(i)].flatten()))
+        lagged = np.concatenate((lagged, predicted_lagged[index(i)].flatten()))
         lstm = torch.cat((lstm, predicted_lstm[index(i)].flatten().to("cpu")))
         gru = torch.cat((gru, predicted_gru[index(i)].flatten().to("cpu")))
         cnn_lstm = torch.cat(
@@ -57,29 +60,31 @@ def plot_every_model(
 
     x = np.arange((end_day - start_day) * 24) + hour_offset
 
+    plt.figure(figsize=(8, 5))
     plt.plot(x, ground_truth, label="Ground Truth")
-    plt.plot(x, naive, label="Naive Baseline")
+    plt.plot(x, naive, label="Naive Baseline", linewidth=1)
+    plt.plot(x, lagged, label="24-h Lagged Baseline", linewidth=1)
     plt.plot(x, lstm, label="LSTM", linewidth=1)
     plt.plot(x, gru, label="GRU", linewidth=1)
     plt.plot(x, cnn_lstm, label="CNN-LSTM", linewidth=1)
-    for day in range(end_day - start_day):
-        plt.axvline(
-            x=24 * (day + 1),
-            ls=":",
-            ymin=0.05,
-            ymax=0.95,
-            color="gray",
-        )
-
+    plt.axvline(
+        x=24 + hour_offset,
+        ls=":",
+        ymin=0.05,
+        ymax=0.95,
+        color="gray",
+    )
     plt.axhline(y=0, color="grey")
 
     plt.xticks(
         np.arange(0 + hour_offset, (end_day - start_day) * 24 + hour_offset + 1, 3)
     )
 
+    side = "left" if hour_offset == 0 else "right"
+    plt.legend(loc=f"upper {side}")
+
     plt.xlabel("Hour")
     plt.ylabel("Relative Consumption")
-    plt.legend()
     if outfile:
         plt.savefig(outfile)
     plt.show()
